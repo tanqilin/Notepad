d7d4a4f0a34144fbf4957140ecb25ba4549e926a
Admin <1135574399@qq.com>
2017-09-04 00:03:19 +0800

    新增启动页面

--- app/src/main/java/com/admin/notepad/MainActivity.java
+++ app/src/main/java/com/admin/notepad/MainActivity.java
@@ -1,14 +1,40 @@
 package com.admin.notepad;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import com.admin.notepad.index.IndexActivity;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-       // 测试
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // 测试
+        if(true){
+            setContentView(R.layout.activity_main);
+            new Handler().postDelayed(r, 5000);
+        }else{
+            setContentView(R.layout.app_start_page);
+        }
+    }
+
+    // 定时跳转页面
+    Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            IndexActivity.actionStart(MainActivity.this);
+            finish();
+        }
+    };
+
+    @Override
+    public void onClick(View v){
+
     }
 }
